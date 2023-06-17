ab0076ffff9fc82c9554246b8b653f73b6b1e8e3
poly000 <1348292515@qq.com>
2023-06-17 21:48:56 +0800

    淫趴会议！

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 extern crate google_calendar3 as calendar3;
 
-use calendar3::api::Calendar;
-use calendar3::client::Hub;
-use calendar3::hyper::service::Service;
-use calendar3::hyper::Uri;
-use calendar3::oauth2::storage::TokenStorage;
-use calendar3::{chrono, hyper, hyper_rustls, oauth2, CalendarHub, FieldMask};
-use calendar3::{Error, Result};
+use calendar3::api::{ConferenceData, ConferenceSolutionKey, Event, EventDateTime};
+use calendar3::chrono::{DateTime, Utc};
+use calendar3::hyper::client::HttpConnector;
+use calendar3::hyper_rustls::HttpsConnector;
+use calendar3::oauth2::authenticator::Authenticator;
+use calendar3::{hyper, hyper_rustls, oauth2, CalendarHub};
 use std::default::Default;
+use tracing::{error, info};
 
 #[derive(serde::Deserialize, serde::Serialize)]
 struct Installed {
@@ -16,16 +16,13 @@ struct Installed {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<(), anyhow::Error> {
-    // Get an ApplicationSecret instance by some means. It contains the `client_id` and
-    // `client_secret`, among other things.
+    tracing_subscriber::fmt().init();
+
     let Installed { installed: secret } =
         serde_json::from_str(include_str!("../client_secret.json"))?;
 
-    // Instantiate the authenticator. It will choose a suitable authentication flow for you,
-    // unless you replace  `None` with the desired Flow.
-    // Provide your own `AuthenticatorDelegate` to adjust the way it operates and get feedback about
-    // what's going on. You probably want to bring in your own `TokenStorage` to persist tokens and
-    // retrieve them from storage.
+    info!("Starting auth...");
+
     let auth = oauth2::InstalledFlowAuthenticator::builder(
         secret,
         oauth2::InstalledFlowReturnMethod::HTTPRedirect,
@@ -33,7 +30,38 @@ async fn main() -> anyhow::Result<(), anyhow::Error> {
     .persist_tokens_to_disk("oauth_status.json")
     .build()
     .await?;
-    let hub = CalendarHub::new(
+
+    let hub = build_calendar_hub(auth);
+
+    let now = Utc::now();
+    let req = make_meet_conf("淫趴", now, now, "Asia/Shanghai");
+
+    let result = hub
+        .events()
+        .insert(req, "primary")
+        .supports_attachments(true)
+        .send_notifications(true)
+        .conference_data_version(1)
+        .doit()
+        .await;
+
+    let Ok(res) = result else {
+        let e = result.unwrap_err();
+        error!("{e}");
+        return Err(e)?;
+    };
+
+    // res.1.hangout_link;
+
+    info!("ok: {res:?}");
+
+    Ok(())
+}
+
+fn build_calendar_hub(
+    auth: Authenticator<HttpsConnector<HttpConnector>>,
+) -> CalendarHub<HttpsConnector<HttpConnector>> {
+    CalendarHub::new(
         hyper::Client::builder().build(
             hyper_rustls::HttpsConnectorBuilder::new()
                 .with_native_roots()
@@ -43,33 +71,47 @@ async fn main() -> anyhow::Result<(), anyhow::Error> {
                 .build(),
         ),
         auth,
-    );
+    )
+}
 
-    let result = {
-        let title = "淫趴";
-        let mut req = Calendar::default();
-        req.summary = Some(title.into());
+fn unique_id() -> String {
+    use rand::distributions::{Alphanumeric, Distribution};
+    use rand::thread_rng;
 
-        hub.calendars().insert(req).doit().await
-    };
+    let mut rng = thread_rng();
+    Alphanumeric
+        .sample_iter(&mut rng)
+        .take(32)
+        .map(char::from)
+        .collect()
+}
 
-    match result {
-        Err(e) => match e {
-            // The Error enum provides details about what exactly happened.
-            // You can also just use its `Debug`, `Display` or `Error` traits
-            Error::HttpError(_)
-            | Error::Io(_)
-            | Error::MissingAPIKey
-            | Error::MissingToken(_)
-            | Error::Cancelled
-            | Error::UploadSizeLimitExceeded(_, _)
-            | Error::Failure(_)
-            | Error::BadRequest(_)
-            | Error::FieldClash(_)
-            | Error::JsonDecodeError(_, _) => println!("{}", e),
-        },
-        Ok(res) => println!("Success: {:?}", res),
-    }
-
-    Ok(())
+fn make_meet_conf(
+    title: impl Into<String>,
+    start_time: DateTime<Utc>,
+    end_time: DateTime<Utc>,
+    timezone: impl Into<String> + Clone,
+) -> Event {
+    let mut req = Event::default();
+    let mut conf_data = ConferenceData::default();
+    conf_data.create_request = Some(calendar3::api::CreateConferenceRequest {
+        conference_solution_key: Some(ConferenceSolutionKey {
+            type_: Some("hangoutsMeet".into()),
+        }),
+        request_id: Some(unique_id()),
+        ..Default::default()
+    });
+    req.conference_data = Some(conf_data);
+    req.summary = Some(title.into());
+    req.start = Some(EventDateTime {
+        date_time: Some(start_time),
+        time_zone: Some(timezone.to_owned().into()),
+        ..Default::default()
+    });
+    req.end = Some(EventDateTime {
+        date_time: Some(end_time),
+        time_zone: Some(timezone.into()),
+        ..Default::default()
+    });
+    req
 }
